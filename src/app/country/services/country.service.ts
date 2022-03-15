6b2faf6820c88ed56d9f4c9da122f92ce746ef10
Raquel Villaverde Palma <raquelv85@gmail.com>
2022-03-15 17:10:33 +0100

    create interface to country response

--- src/app/country/services/country.service.ts
+++ src/app/country/services/country.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Country } from '../interfaces/country.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -11,9 +12,9 @@ export class CountryService {
 
   constructor( private http: HttpClient) { }
 
-  searchCountry( country: string): Observable<any>{
+  searchCountry( country: string): Observable<Country[]>{
     const url = `${this.apiUrl}/name/${country}`
-    return this.http.get( url )
+    return this.http.get<Country[]>( url )
   }
 
 }
